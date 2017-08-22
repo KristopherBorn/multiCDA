4e91b3d7e49ca9ec0c5d5a382d8e6ef9707a1ab3
Daniel Strüber <strueber@mathematik.uni-marburg.de>
2017-08-22 19:28:30 +0200

    Made duplicate edge elimination in MultiCDA optional

--- org.eclipse.emf.henshin.cpa.atomic/src/org/eclipse/emf/henshin/cpa/atomic/PushoutResult.java
+++ org.eclipse.emf.henshin.cpa.atomic/src/org/eclipse/emf/henshin/cpa/atomic/PushoutResult.java
@@ -21,7 +21,9 @@ import org.eclipse.emf.henshin.model.Rule;
 // Generell: muss die matches m1 und m2 aus L1 und L2 enthalten und somit auch G.
 // daher kennt es oder referenziert es auch (indirekt?) die beiden Regeln
 public class PushoutResult {
-	
+
+	private static final boolean DELETE_DUPLICATE_EDGES = false;
+
 	HenshinFactory henshinFactory = HenshinFactory.eINSTANCE;
 
 	/**
@@ -59,23 +61,27 @@ public class PushoutResult {
 	private HashMap<Node, Node> mappingsOfRule2;
 
 	public PushoutResult(Rule rule1, Span s1span, Rule rule2) {
-		
-//		TODO: prüfen, dass alle mappings in die beiden Regeln verweisen, bzw. keine der Regeln NULL ist. Sonst werfen einer Exception!
-//		throw new IllegalStateException("blabla")
-//		ggf. in "static" Methode (Span) s1span.isValid() auslagern die eine "IllegalStateException" wirft 
-//		s1span.validate(rule1, rule2);
+
+		// TODO: prüfen, dass alle mappings in die beiden Regeln verweisen, bzw.
+		// keine der Regeln NULL ist. Sonst werfen einer Exception!
+		// throw new IllegalStateException("blabla")
+		// ggf. in "static" Methode (Span) s1span.isValid() auslagern die eine
+		// "IllegalStateException" wirft
+		// s1span.validate(rule1, rule2);
 		AtomicCoreCPA.checkNull(rule1);
 		AtomicCoreCPA.checkNull(s1span);
 		AtomicCoreCPA.checkNull(rule2);
-		if(!s1span.validate(rule1, rule2))
-			throw new IllegalArgumentException("Span is in invalide state.");			
+		if (!s1span.validate(rule1, rule2))
+			throw new IllegalArgumentException("Span is in invalide state.");
 
-//        Predicate<Object> a = Objects::nonNull; // AHA Prädikate - keine Ahnung!
-//		Objects::nonNull(rule1); // bringt nichts, nur boolscher Rückgabewert!
-//		Objects.nonNull(rule2); // bringt nichts, nur boolscher Rückgabewert!
-		
+		// Predicate<Object> a = Objects::nonNull; // AHA Prädikate - keine
+		// Ahnung!
+		// Objects::nonNull(rule1); // bringt nichts, nur boolscher
+		// Rückgabewert!
+		// Objects.nonNull(rule2); // bringt nichts, nur boolscher Rückgabewert!
+
 		Graph l1 = rule1.getLhs();
-		mappingsOfRule1 = new HashMap<Node,Node>();
+		mappingsOfRule1 = new HashMap<Node, Node>();
 		Copier copierForRule1 = new Copier();
 		Graph pushout = (Graph) copierForRule1.copy(l1);
 		copierForRule1.copyReferences();
@@ -84,7 +90,7 @@ public class PushoutResult {
 			mappingsOfRule1.put(node, copyResultNode);
 		}
 		Graph l2 = rule2.getLhs();
-		mappingsOfRule2 = new HashMap<Node,Node>();
+		mappingsOfRule2 = new HashMap<Node, Node>();
 		Copier copierForRule2 = new Copier();
 		Graph l2copy = (Graph) copierForRule2.copy(l2);
 		copierForRule2.copyReferences();
@@ -98,61 +104,66 @@ public class PushoutResult {
 		for (Node node : s1.getNodes()) {
 			// retarget associated edges
 			// get associated node and mapping in both copies
-			if (s1span.getMappingIntoRule2(node) == null) { //TODO: remove!
+			if (s1span.getMappingIntoRule2(node) == null) { // TODO: remove!
 				System.out.println("bla");
 			}
-			
-			Node l1node  = s1span.getMappingIntoRule1(node).getImage();
+
+			Node l1node = s1span.getMappingIntoRule1(node).getImage();
 			Node l2node = s1span.getMappingIntoRule2(node).getImage();
-			
-			
+
 			if (l1node == null || l2node == null) {
 				System.out.println("Did not find a L1 or L2 counterpart for one of the nodes in S1!");
-//				TODO: Exception werfen! 
+				// TODO: Exception werfen!
 			} else {
 				Node mergedNode = mappingsOfRule1.get(l1node);
-				Node discardNode =  mappingsOfRule2.get(l2node);
-				mergedNode.setName(mergedNode.getName()+","+discardNode.getName());
+				Node discardNode = mappingsOfRule2.get(l2node);
+				mergedNode.setName(mergedNode.getName() + "," + discardNode.getName());
 
 				Set<Edge> duplicateEdgesToDelete = new HashSet<Edge>();
-				
+
 				List<Edge> l2nodesIncoming = new LinkedList<Edge>(discardNode.getIncoming());
 				for (Edge eIn : l2nodesIncoming) {
-					 // hier prüfen, ob es zwischen den beiden Knoten bereits eine Kante des Typs gibt. Dann keine weitere Kante erzeugen!
-					if(mergedNode.getIncoming(eIn.getType(), eIn.getSource()) != null){
-						/*löschen der Kante*/
+					// hier prüfen, ob es zwischen den beiden Knoten bereits
+					// eine Kante des Typs gibt. Dann keine weitere Kante
+					// erzeugen!
+					if (mergedNode.getIncoming(eIn.getType(), eIn.getSource()) != null) {
+						/* löschen der Kante */
 						duplicateEdgesToDelete.add(eIn);
-					}else{  
+					} else {
 						eIn.setTarget(mergedNode);
 					}
 				}
 
 				List<Edge> l2nodesOutgoing = new LinkedList<Edge>(discardNode.getOutgoing());
-				for (Edge eOut : l2nodesOutgoing) { 
-					 // hier prüfen, ob es zwischen den beiden Knoten bereits eine Kante des Typs gibt. Dann keine weitere Kante erzeugen!
-					if(mergedNode.getOutgoing(eOut.getType(), eOut.getTarget()) != null){
-						/*löschen der Kante*/
+				for (Edge eOut : l2nodesOutgoing) {
+					// hier prüfen, ob es zwischen den beiden Knoten bereits
+					// eine Kante des Typs gibt. Dann keine weitere Kante
+					// erzeugen!
+					if (mergedNode.getOutgoing(eOut.getType(), eOut.getTarget()) != null) {
+						/* löschen der Kante */
 						duplicateEdgesToDelete.add(eOut);
-					}else{
+					} else {
 						eOut.setSource(mergedNode);
 					}
 				}
-				
+
 				mappingsOfRule2.put(l2node, mergedNode);
-				
+
 				if (discardNode.getAllEdges().size() > 0) {
-					System.err.println("All Edges of should have been removed, but still "
-							+ l2node.getAllEdges().size() + " are remaining!");
+					System.err.println("All Edges of should have been removed, but still " + l2node.getAllEdges().size()
+							+ " are remaining!");
 				}
-				
+
 				// löschen des Knoten "nodeInL2y"
 				Graph graphOfNodeL2 = discardNode.getGraph();
 				boolean removedNode = graphOfNodeL2.removeNode(discardNode);
 				System.err.println("removedNode: " + removedNode);
-				
-				// löschen der doppelten Kanten 
-				for(Edge edgeToDelete : duplicateEdgesToDelete){
-					graphOfNodeL2.removeEdge(edgeToDelete);
+
+				// löschen der doppelten Kanten
+				if (DELETE_DUPLICATE_EDGES) {
+					for (Edge edgeToDelete : duplicateEdgesToDelete) {
+						graphOfNodeL2.removeEdge(edgeToDelete);
+					}
 				}
 			}
 		}
@@ -165,20 +176,18 @@ public class PushoutResult {
 		for (Edge edgeInCopyOfLhsOfRule2 : edgesInCopyOfLhsOfRule2) {
 			edgeInCopyOfLhsOfRule2.setGraph(pushout);
 		}
-		
+
 		// check that NO edges and nodes are remaining in copyOfLhsOfRule2
 		if (l2copy.getEdges().size() > 0) {
-			System.err.println(l2copy.getEdges().size() + " edges reaming in " + l2copy
-					+ ", but should be 0");
+			System.err.println(l2copy.getEdges().size() + " edges reaming in " + l2copy + ", but should be 0");
 		}
 		if (l2copy.getNodes().size() > 0) {
-			System.err.println(l2copy.getNodes().size() + " nodes reaming in " + l2copy
-					+ ", but should be 0");
+			System.err.println(l2copy.getNodes().size() + " nodes reaming in " + l2copy + ", but should be 0");
 		}
-		
-		// check that the number of edges and nodes in edgeInCopyOfLhsOfRule1 is correct
-		int numberOfExpectedNodes = (l1.getNodes().size() + l2.getNodes().size()
-				- s1.getNodes().size());
+
+		// check that the number of edges and nodes in edgeInCopyOfLhsOfRule1 is
+		// correct
+		int numberOfExpectedNodes = (l1.getNodes().size() + l2.getNodes().size() - s1.getNodes().size());
 		if (pushout.getNodes().size() != numberOfExpectedNodes) {
 			System.err.println("Amount of nodes in created result graph of pushout not as expected. Difference: "
 					+ (pushout.getNodes().size() - numberOfExpectedNodes));
@@ -187,7 +196,7 @@ public class PushoutResult {
 		resultGraph = pushout;
 
 	}
-	
+
 	private Mapping getMappingOfOrigin(List<Mapping> mappingsOfRules, Node origin) {
 		for (Mapping mapping : mappingsOfRules) {
 			if (mapping.getOrigin() == origin)
