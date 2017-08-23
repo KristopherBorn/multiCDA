b6c1008f5df447bdf85e337b365a0fd5327f7e33
kborn <kristopherborn@gmx.de>
2017-08-23 08:37:05 +0200

    removed superfluous comments and adapted expected results

--- org.eclipse.emf.henshin.cpa.atomic/src/org/eclipse/emf/henshin/cpa/atomic/unitTest/BoundaryNodesTest.java
+++ org.eclipse.emf.henshin.cpa.atomic/src/org/eclipse/emf/henshin/cpa/atomic/unitTest/BoundaryNodesTest.java
@@ -54,37 +54,6 @@ public class BoundaryNodesTest {
 				executeNonLoop);
 		Assert.assertEquals(3, computedConflictAtoms.size());
 		
-		Set<ConflictAtom> methodCAs = new HashSet<ConflictAtom>();
-		
-		ConflictAtom conflictAtom_c_2_5 = null;
-		ConflictAtom conflictAtom_c_8_9 = null;
-		ConflictAtom conflictAtom_n_9_10 = null;
-
-		int numberOf_METHOD_atoms = 0;
-		int numberOf_PARAMETER_atoms = 0;
-		for (ConflictAtom conflictAtom : computedConflictAtoms) {
-//				Span span = conflictAtom.getSpan();
-//				Graph graph = span.getGraph();
-//				EList<Node> nodes = graph.getNodes();
-//				for (Node nodeInOverlapGraph : nodes) {
-//					if (nodeInOverlapGraph.getType().getName().equals("Method")) {
-//						numberOf_METHOD_atoms++;
-//						if(nodeInOverlapGraph.getName().contains("13"))
-//							conflictAtom_Method_2_13 = conflictAtom;
-//						if(nodeInOverlapGraph.getName().contains("14"))
-//							conflictAtom_Method_3_14 = conflictAtom;
-//					} else if (nodeInOverlapGraph.getType().getName().equals("Parameter")) {
-//						numberOf_PARAMETER_atoms++;
-//						if(nodeInOverlapGraph.getName().contains("15"))
-//							conflictAtom_Parameter_5_15 = conflictAtom;
-//					} else {
-//						assertTrue("node of wrong type in overlap graph", false);
-//					}
-//				}
-		}
-//			assertEquals(2, numberOf_METHOD_atoms);
-//			assertEquals(1, numberOf_PARAMETER_atoms);
-		
 		Set<Span> allMinimalConflictReasons = new HashSet<Span>();
 		//TODO: check that the two Reasons had been found AND that the three ConflictAtoms have three MCR (minimal conflict reasons)!
 		for(ConflictAtom conflictAtom : computedConflictAtoms){
@@ -93,12 +62,6 @@ public class BoundaryNodesTest {
 			allMinimalConflictReasons.addAll(reasons);
 		}
 		Assert.assertEquals(3, allMinimalConflictReasons.size());
-		
-//			Span initialReasonOfMethod_3_14_Atom = conflictAtom_Method_3_14.getReasons().iterator().next();
-//			Span initialReasonOfParameter_5_15_Atom = conflictAtom_Parameter_5_15.getReasons().iterator().next();
-//		System.out.println(initialReasonOfMethod_3_14_Atom);
-//		System.out.println(initialReasonOfParameter_5_15_Atom);
-//			Assert.assertTrue(initialReasonOfMethod_3_14_Atom.equals(initialReasonOfParameter_5_15_Atom));
 		
 		Set<MinimalConflictReason> minimalConflictReasons = new HashSet<MinimalConflictReason>();
 		for(Span minimalConflictReason : allMinimalConflictReasons){
@@ -132,16 +95,13 @@ public class BoundaryNodesTest {
 			System.out.println(conflictReason.toShortString());
 		}
 		Assert.assertEquals(3, originMCRs.size());
-		Assert.assertEquals(29, conflictReasonsDerivedFromInitialReason.size());
+		Assert.assertEquals(49, conflictReasonsDerivedFromInitialReason.size());
 		
 		Set conflictAtoms = new HashSet<>();
 		for(MinimalConflictReason mcr : originMCRs){
 			conflictAtoms.addAll(mcr.getOriginMCRs());
 		}
 		Assert.assertEquals(3, conflictAtoms.size());
-		
-		
-		
 
 	}
 
@@ -162,7 +122,7 @@ public class BoundaryNodesTest {
 		}
 		List<CriticalPair> criticalPairs = essCPs.getCriticalPairs();
 		
-		Assert.assertEquals(29, criticalPairs.size());
+		Assert.assertEquals(49, criticalPairs.size());
 		// die 29 Ergebnisse sind die verbleibenden Ergebnisse der ursprünglich 49 Ergebnsise,
 		// nach Abzug der Ergebnisse mit parallelen Kanten
 	}
