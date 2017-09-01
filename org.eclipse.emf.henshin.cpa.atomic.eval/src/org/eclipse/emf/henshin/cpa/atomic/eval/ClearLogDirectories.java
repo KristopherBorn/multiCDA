f5b088e79e37594cd6f2f42232ab51ed9d65f341
Daniel Strüber <strueber@mathematik.uni-marburg.de>
2017-09-01 15:32:16 +0200

    Add "ClearLogDirectories", which delets all files in log directories

---
+++ org.eclipse.emf.henshin.cpa.atomic.eval/src/org/eclipse/emf/henshin/cpa/atomic/eval/ClearLogDirectories.java
@@ -0,0 +1,26 @@
+package org.eclipse.emf.henshin.cpa.atomic.eval;
+
+import java.io.File;
+
+public class ClearLogDirectories {
+	public static void main(String[] args) {
+		File file1 = new File("logs/results");
+		File file2 = new File("logs/time");
+		deleteFolder(file1);
+		deleteFolder(file2);
+		System.out.println("Cleared log directories.");
+	}
+	
+	public static void deleteFolder(File folder) {
+	    File[] files = folder.listFiles();
+	    if(files!=null) { //some JVMs return null for empty dirs
+	        for(File f: files) {
+	            if(f.isDirectory()) {
+	                deleteFolder(f);
+	            } else {
+	                f.delete();
+	            }
+	        }
+	    }
+	}
+}
